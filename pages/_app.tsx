2a713735f4cab5cbe3d7e9f6530de2685d59980b
Kang, Zixuan <zkang35@gatech.edu>
2021-12-13 19:29:42 -0500

    Remove config setting

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,7 @@
 import "../styles/globals.scss";
 import type { AppProps } from "next/app";
 import Navigation from "../components/Navigation";
-import { config } from "@fortawesome/fontawesome-svg-core";
 import "@fortawesome/fontawesome-svg-core/styles.css";
-
-config.autoAddCss = false;
 
 const Portfolio = ({ Component, pageProps }: AppProps): JSX.Element => {
   return (
