99d0b60e20e47401304cb3c66a3a6706f44767df
Harry Kang <harry.z.kang@outlook.com>
2023-03-05 14:10:07 -0700

    Add Clipboard icon on contactMe page

--- pages/contactMe.tsx
+++ pages/contactMe.tsx
@@ -1,9 +1,29 @@
+import { useState } from "react";
 import { NextPage } from "next";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCheck, faClipboard } from "@fortawesome/free-solid-svg-icons";
 import Head from "next/head";
 import Header from "../components/Header";
 import contactMeStyles from "../styles/contactMe.module.scss";
 
 const Contact: NextPage = (): JSX.Element => {
+  const [phoneNumberCopied, setPhoneNumberCopied] = useState<Boolean>(false);
+  const [addressCopied, setAddressCopied] = useState<Boolean>(false);
+
+  const copyPhoneNumber = async () => {
+    await navigator.clipboard.writeText("+14703987810");
+    setPhoneNumberCopied(true);
+    setTimeout(() => setPhoneNumberCopied(false), 1000);
+  };
+
+  const copyAddress = async () => {
+    await navigator.clipboard.writeText(
+      "Apt. 1528, 200 W Monroe St., Phoenix, AZ, 85003-1763"
+    );
+    setAddressCopied(true);
+    setTimeout(() => setAddressCopied(false), 1000);
+  };
+
   return (
     <div className="container">
       <Head>
@@ -14,33 +34,37 @@ const Contact: NextPage = (): JSX.Element => {
         smallHeading="This is how you can reach me..."
       />
       <div className={contactMeStyles.boxes}>
-        <div>
+        <div className={contactMeStyles.boxes__box}>
           <a href="mailto:harry.z.kang@outlook.com">
             <span className="text-secondary">Email: </span>
             harry.z.kang@outlook.com
           </a>
         </div>
-        <div>
-          <span
-            className="text-secondary"
-            onClick={() => navigator.clipboard.writeText("+14703987810")}
-          >
-            Phone Number:{" "}
-          </span>
+        <div
+          className={contactMeStyles.boxes__box}
+          onClick={async () => await copyPhoneNumber()}
+        >
+          <FontAwesomeIcon
+            size="1x"
+            color={phoneNumberCopied ? "green" : "black"}
+            icon={phoneNumberCopied ? faCheck : faClipboard}
+            className={contactMeStyles.boxes__box__clip}
+          />
+          <span className="text-secondary">Phone Number: </span>
           +1 (470) 398-7810
         </div>
-        <div>
-          <span
-            className="text-secondary"
-            onClick={() =>
-              navigator.clipboard.writeText(
-                "200 W Monroe St., Phoenix, AZ, 85003-1763"
-              )
-            }
-          >
-            Address:{" "}
-          </span>
-          200 W Monroe St., Phoenix, AZ, 85003-1763
+        <div
+          className={contactMeStyles.boxes__box}
+          onClick={async () => await copyAddress()}
+        >
+          <FontAwesomeIcon
+            size="1x"
+            color={addressCopied ? "green" : "black"}
+            icon={addressCopied ? faCheck : faClipboard}
+            className={contactMeStyles.boxes__box__clip}
+          />
+          <span className="text-secondary">Address: </span>
+          Apt 1528, 200 W Monroe St., Phoenix, AZ, 85003-1763
         </div>
       </div>
     </div>
